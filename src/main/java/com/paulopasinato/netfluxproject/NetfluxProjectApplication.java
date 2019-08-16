be6e74b891968bfb200c1a29f164d22ca46ac3aa
paulopasinato <paulo.pasinato@amedigital.com>
2019-08-16 16:21:43 -0300

    first commit

---
+++ src/main/java/com/paulopasinato/netfluxproject/NetfluxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.paulopasinato.netfluxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfluxProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulopasinato/netfluxproject/NetfluxProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paulopasinato.netfluxproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetfluxProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
